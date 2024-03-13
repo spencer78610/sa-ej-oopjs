168294fa2f95fc48ea8d600189549f4a614dc674
EthanJ <e_jutila@fanshaweonline.ca>
2024-03-12 23:25:44 -0400

    updated html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-function startGame() {
-    document.getElementById('start-menu').style.display = 'none';
-    document.querySelector('.game-board').style.display = 'grid';
-  }
-(() => {
-    
-  
-})();
 
